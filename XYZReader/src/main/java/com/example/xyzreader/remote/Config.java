ae570b06b3d1c1c32fcc8fdcba109aa784848e39
srv-twry <saurav007tiwary@gmail.com>
2017-06-15 04:51:37 +0530

    Improved the list activity

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -3,13 +3,15 @@ package com.example.xyzreader.remote;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static android.support.v4.content.res.TypedArrayUtils.getString;
+
 public class Config {
     public static final URL BASE_URL;
 
     static {
         URL url = null;
         try {
-            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+            url = new URL("https://nspf.github.io/XYZReader/data.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
         }
